319df0bb30ea8813600788410d61c15ccd6cbec1
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2025-05-07 16:40:07 +0200

    inp

--- credentials/git_crypt_uncrypt.sh
+++ credentials/git_crypt_uncrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -n "$GIT_CRYPT_KEY" ]; then
+if [ -z "$GIT_CRYPT_KEY" ]; then
     echo "GIT_CRYPT_KEY env variable is not defined"
     exit 1
 fi
 
-echo $GIT_CRYPT_KEY | base64 -d > .git-crypt.key
+echo "$GIT_CRYPT_KEY" | base64 -d > .git-crypt.key
 git-crypt unlock .git-crypt.key
