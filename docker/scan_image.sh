811099c71fe870f91b6106976c953a40e6e3184e
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2026-01-22 15:29:22 +0100

    trivy change

--- docker/scan_image.sh
+++ docker/scan_image.sh
@@ -4,4 +4,4 @@ set -e
 
 source ./devops/ci_cd_vars.sh
 
-exec trivy image --platform linux/amd64 --severity CRITICAL --exit-code 1 --quiet "${IMAGE_URL}"
+trivy image --server http://trivy-server:4954 --severity CRITICAL --exit-code 1 --quiet "${IMAGE_URL}"
